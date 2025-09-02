4ccabd19c0446071a44e6f283f236859d4fc957c
kira <coledt54@gmail.com>
2025-09-02 15:44:45 -0700

    hi

--- flake.nix
+++ flake.nix
@@ -6,10 +6,7 @@
   };
 
   outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    system = "x86_64-linux";
 
   };
 }
