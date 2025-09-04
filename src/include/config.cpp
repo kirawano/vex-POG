f76d68e14d3a27b6e9732bb68d91c6fbe5a52a2f
Silii <klngyn9331@gmail.com>
2025-09-04 15:17:49 -0700

    Update config.cpp

--- src/include/config.cpp
+++ src/include/config.cpp
@@ -1,1 +1,8 @@
-
+// ============ CONFIG ============
+// Update these to match robot ports and directions.
+constexpr int LF_PORT = 1;  // Left front
+constexpr int LM_PORT = 2;  // Left back/middle
+constexpr int LR_PORT = 3;  // Left top/rear
+constexpr int RF_PORT = 4;  // Right front
+constexpr int RM_PORT = 5;  // Right back/middle
+constexpr int RR_PORT = 6;  // Right top/rear
