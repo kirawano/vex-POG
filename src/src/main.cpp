5238d6860c975d9b93dfe6c18b17d9d07b1dd2bb
Silii <klngyn9331@gmail.com>
2025-09-04 15:17:36 -0700

    Update main.cpp

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,14 +1,6 @@
 #include "main.h"
 #include <cmath>
-
-// ============ CONFIG ============
-// Update these to match robot ports and directions.
-constexpr int LF_PORT = 1;  // Left front
-constexpr int LM_PORT = 2;  // Left back/middle
-constexpr int LR_PORT = 3;  // Left top/rear
-constexpr int RF_PORT = 4;  // Right front
-constexpr int RM_PORT = 5;  // Right back/middle
-constexpr int RR_PORT = 6;  // Right top/rear
+#incliude "config.cpp"
 
 constexpr bool LEFT_REVERSED  = false;
 constexpr bool RIGHT_REVERSED = true;   // Typically true for mirrored sides
